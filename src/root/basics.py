801cf1bc3e970c9f4b32f40f677adc31fb7b753a
msanavarro <msarmientonavarro@gmail.com>
2019-09-02 23:11:24 -0500

    Contribución inicial

---
+++ src/root/basics.py
@@ -0,0 +1,21 @@
+'''
+Created on 31 ago. 2019
+
+@author: msanavarro
+
+calculo de gradiente, hessiana, ...
+'''
+
+x = 25
+print(x)
+
+for  i in range(100,110):
+    print(i)
+
+def gradiente(f, x):
+    return x
+
+def hessiana(f, x):
+    return(x)
+
+print(gradiente('a', x))

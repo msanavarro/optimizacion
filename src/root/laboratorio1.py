ea39a10ff279628714b6ec1fab22c0607da17866
msanavarro <msarmientonavarro@gmail.com>
2019-09-09 01:26:27 -0500

    Mínimos cuadrados

--- src/root/laboratorio1.py
+++ src/root/laboratorio1.py
@@ -16,7 +16,9 @@ def mC(X, y):
     """
     Devuelve el estimador de minimos cuadrados de b para y = Xb
 
-    Si la matriz X es de rango completo, devuelve el estimador.
+    Si la matriz X es de rango completo, devuelve el estimador de mínimos 
+    cuadrados de b para el problema y = Xb, que es b=((X'X)^-1)X'y. En caso 
+    contrario regresa 
     
     Requiere numpy y numpy.linalg
 
@@ -33,15 +35,29 @@ def mC(X, y):
         Estimador de minimos cuadrados de b (vector de tamano k)
 
     """
-    if(npl.matrix_rank(X) == np.size(X, 1)):
-        b = np.matmul(npl.inv(np.matmul(np.transpose(X), X)), np.matmul(np.transpose(X), y))
-        print(npl.inv(np.matmul(np.transpose(X), X)))
-        return b
+    if(type(X)==np.ndarray and type(y)==np.ndarray):
+        if(np.size(X,0)==np.size(y,0)):
+            if(npl.matrix_rank(X) == np.size(X, 1)):
+                try:
+                    b = np.matmul(npl.inv(np.matmul(np.transpose(X), X)), np.matmul(np.transpose(X), y))
+                    return b
+                except:
+                    print("Ocurrio un error al calcular b")
+                    return
+            else:
+                print("La matriz X no es de rango completo")
+                return
+        else:
+            print("Las dimensiones de X y de y no concuerdan")
+            return 
     else:
-        print("La matriz X no es de rango completo")
-        return -1
+        print("Los objetos que se recibieron no son del tipo numpy.ndarray")
+        return
+    
 
 X = np.array([[0.0,1.0],[2.0,3.0],[4.0,5.0]])
+#X = np.array([[0.0,1.0],[0.0,1.0],[0.0,1.0]])
+#X='A'
 y = np.array([0.1,0.9,2.4])
 
 print(mC(X, y))

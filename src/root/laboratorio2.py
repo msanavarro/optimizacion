2b6e9b1c62868740c8002f26c5f948892c59d497
msanavarro <msarmientonavarro@gmail.com>
2019-09-19 13:17:05 -0500

    documentación y pdf's labs

--- src/root/laboratorio2.py
+++ src/root/laboratorio2.py
@@ -4,8 +4,13 @@ Created on 10 sep. 2019
 @author: msanavarro
 '''
 
+def gradiente(f, x):
+    return
+
+def hessiana(f, x):
+    return
+
 def metodoNewton(x):
     return x
 
-print(metodoNewton(10
-                   ))
+print(metodoNewton(10))

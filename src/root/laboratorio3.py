b68794de9b5e394ff6e2059898ca1ae3f03a3ce7
msanavarro <msarmientonavarro@gmail.com>
2019-09-24 19:47:17 -0500

    máquinas de soporte vectorial duro

--- src/root/laboratorio3.py
+++ src/root/laboratorio3.py
@@ -3,3 +3,14 @@ Created on 24 sep. 2019
 
 @author: msanavarro
 '''
+
+
+def hard_svm(X, y):
+    '''
+    (X,y): Datos de entrenamiento [X.shape=(m,p), y.shape=(m,)]
+    X,y matrices de numpy
+    (w,b):  Hiperplano [w.shape=(p,), b.shape=(1,)]
+    '''
+    w = 1
+    b = 1
+    return w, b

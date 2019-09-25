be9d5f278fc7299ce0b09abd4db7eac8e52a8146
msanavarro <msarmientonavarro@gmail.com>
2019-09-24 19:50:39 -0500

    máquinas de soporte vectorial duras y generados de puntos separables

--- src/root/laboratorio3.py
+++ src/root/laboratorio3.py
@@ -4,6 +4,7 @@ Created on 24 sep. 2019
 @author: msanavarro
 '''
 
+import numpy as np
 
 def hard_svm(X, y):
     '''
@@ -13,4 +14,17 @@ def hard_svm(X, y):
     '''
     w = 1
     b = 1
+    
+    
+    
     return w, b
+
+def datos(N):
+    np.random.seed(1111)
+    n = int(N/2)
+    cov = [[1, 0], [0, 1]]
+    X1 = np.random.multivariate_normal([-9,10], cov, n)
+    X2 = np.random.multivariate_normal([7,-8], cov, n)
+    X = np.concatenate((X1, X2), axis=0)
+    y = np.concatenate((np.ones(n), -1*np.ones(n)), axis=0)
+    return X, y.squeeze()
